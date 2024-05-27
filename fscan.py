b0473652638fef33d5fb623d4ef2927be5790559
feqtico <73079578+feqtico@users.noreply.github.com>
2024-05-27 14:46:24 -0400

    Add files via upload

---
+++ fscan.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from googlesearch import search
+
+
+print("Welcome To FSCAN - Developed By Feqtico.")
+  
+word = input(" Dork To Scan: ")
+
+arama = word
+  
+for j in search(arama, tld="com", num=30, stop=30, pause=2):
+    print(j)
